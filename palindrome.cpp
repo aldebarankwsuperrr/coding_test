4b89e90acf059946456c944894e5e51752cb26a4
aldebarankwsuperrr <86464689+aldebarankwsuperrr@users.noreply.github.com>
2022-01-09 21:24:00 +0700

    Update palindrome.cpp

--- palindrome.cpp
+++ palindrome.cpp
@@ -3,17 +3,14 @@
 
 bool ispalindrome (bool value, std::string a, int low, int high){
     if (low <= high){
-        value = (a[low] == a[high]) ? true : false;
-        return ispalindrome (value, a, low + 1, high - 1);
+        return (a[low] == a[high]) ? ispalindrome (value, a, low + 1, high - 1) : false;
     }
     return value;
 }
 
 int main (){
     std::string a;
-    
-    std::cin >> a;
-
+    std::getline(std::cin, a);
     std::cout << ((ispalindrome(true, a, 0, a.length()-1) == true) ? "Palindrome" : "No");
     return 0;
 }
